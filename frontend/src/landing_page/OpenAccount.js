1ce0a98e14a422080765735c604b667a24ecc064
Anil : gadheanil57@gmail.com <gadheanil57@gmail.com>
2025-08-25 13:47:22 +0530

    add vercel file

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function OpenAccount() {
+
+        let navigate = useNavigate();
+        function handleClick(){
+            navigate("/signup")     
+        }
+
     return ( 
             <div className="mt-5 mb-5 text-center">
                <h2>Open a FulltradeStack account</h2>
                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades</p>
-                 <button className='btn btn-primary text-white fw-bold p-2 fs-6 mt-3 w-25 w-sm-25 w-md-25 w-lg-20'>
+
+                 <button  onClick={handleClick} className='btn btn-primary text-white fw-bold p-2 fs-6 mt-3 w-25 w-sm-25 w-md-25 w-lg-20'>
                  Sign up for free
                   </button>
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 
 function Hero() {
+ 
+  let navigate = useNavigate();
+
+  function handleClick(){
+     navigate("/signup")
+  }
     return ( 
         <div className='container ' style={{marginBottom:"200px",marginTop:"100px"}}>
          <div className='row text-center d-flex  justify-content-center'>
@@ -10,7 +17,7 @@ function Hero() {
 
                 <h1 className=' mt-5'>Invest in everything</h1>
                 <p className=' fs-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                  <button className='btn btn-primary text-white fw-bold p-2 fs-6 mt-3 w-25 w-sm-50 w-md-25 w-lg-20'>
+                  <button onClick={handleClick} className='btn btn-primary text-white fw-bold p-2 fs-6 mt-3 w-25 w-sm-50 w-md-25 w-lg-20'>
                         Sign up for free
                    </button>
 
